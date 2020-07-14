0d85ceaab31f674f4a41b98c010a99113cfca700
7th-Hokage <shubham3sept@gmail.com>
2020-07-14 14:15:21 +0530

    js basic logical assignments done

--- Assignments/javascript assignments/script.js
+++ Assignments/javascript assignments/script.js
@@ -15,3 +15,36 @@ function max(num1,num2,num3){
     else
         return num3;
 }
+function numHeads(num){
+    var count=0;
+    for(i=0;i<num;i++)
+        if(flip()=='H')
+            count++;
+    return count;
+}
+function headsRatio(num){
+    return numHeads(num)/num;
+}
+function flip(){
+    if(Math.floor(Math.random() * 10)%2 == 0)
+        return 'H';
+    else
+        return 'T';
+}
+function padChars(num,str){
+    if(isNaN(num))
+        return "Enter valid Number";
+    else
+        return str.repeat(num);
+}
+function numRollsToGetSix(){
+    var count=0;
+    while((Math.floor(Math.random() * 6) + 1)!=6){
+        count++;
+    }
+    return count;
+}
+if(flip()=='H')
+    document.getElementById("message").innerHTML = "Have a GOOD day!";
+else
+    document.getElementById("message").innerHTML = "Have a BAD day!";
